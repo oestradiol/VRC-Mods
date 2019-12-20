97906ecd2b747ec9f4087f8320e0c2b151520e27
nitro <31241699+nitrog0d@users.noreply.github.com>
2019-12-20 03:54:15 -0300

    Add new buttons and make proper code I guess

---
+++ Buttons/BaseButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace VRCDesktopCamera.Buttons {
+    public class BaseButton {
+        public Transform button;
+        
+        public static Vector3 getButtonPositionFor(int x, int y) {
+            return new Vector3(-630f + (x * 420f), 1050f + (y * -420f));
+        }
+
+        public void setButtonPosition(int x, int y) {
+            button.localPosition = getButtonPositionFor(x, y);
+        }
+
+        public void setTooltip(string tooltip) {
+            button.GetComponent<UiTooltip>().text = tooltip;
+            button.GetComponent<UiTooltip>().alternateText = tooltip;
+        }
+
+        public void setInteractable(bool interactable) {
+            button.gameObject.GetComponent<Button>().interactable = interactable;
+        }
+
+    }
+}
---
+++ Buttons/SingleButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+namespace VRCDesktopCamera.Buttons {
+    class SingleButton : BaseButton {
+        public SingleButton(string text, string tooltip, int x, int y, Transform childOf = null, UnityAction action = null) {
+            button = Object.Instantiate(InstanceUtils.SingleButtonTemplate(), childOf);
+            button.name = "SingleButton";
+            setText(text);
+            setTooltip(tooltip);
+            setButtonPosition(x, y);
+            setAction(action);
+        }
+
+        public void setText(string text) {
+            button.GetComponentInChildren<Text>().text = text;
+        }
+
+        public void setAction(UnityAction action) {
+            button.GetComponent<Button>().onClick = new Button.ButtonClickedEvent();
+            button.GetComponent<Button>().onClick.AddListener(action);
+        }
+    }
+}
