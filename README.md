710565f43cc4df7ef109b418d5583ff6a39ee674
Lucas <onitrog0d@gmail.com>
2021-02-21 20:26:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# TrackingRotator
+[![GitHub All Releases](https://img.shields.io/github/downloads/nitrog0d/TrackingRotator/total?style=for-the-badge)](https://github.com/nitrog0d/TrackingRotator/releases)
+[![GitHub release (latest by date)](https://img.shields.io/github/v/release/nitrog0d/TrackingRotator?style=for-the-badge)](https://github.com/nitrog0d/TrackingRotator/releases/latest)
+[![Support me on Patreon](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fwww.patreon.com%2Fapi%2Fcampaigns%2F1177520&query=data.attributes.patron_count&suffix=%20Patrons&color=FF5441&label=Patreon&logo=Patreon&logoColor=FF5441&style=for-the-badge)](https://patreon.com/nitrog0d)  
+
+Video preview soon.
+
+A mod that lets you rotate your tracking, it was made with "play while laying down" in mind.
+
+* **Warning:** The VRChat team is not very keen on modding or reverse engineering the game, while the mod does not include anything that would ruin the fun for others, using it may still be a bannable offence.
+
+* **USE IT AT YOUR OWN RISK**, I am not responsible for any bans or any punishments you may get by using this mod!
+
+## Installation
+* **Make sure you have run the [MelonLoader Installer](https://github.com/LavaGang/MelonLoader.Installer/releases/latest/download/MelonLoader.Installer.exe) by [LavaGang](https://github.com/LavaGang) first (feel free to join the [VRChat Modding Group Discord](https://discord.gg/jgvc9Fd) for help!).**
+* Download the [latest version](https://github.com/nitrog0d/TrackingRotator/releases/latest/download/TrackingRotator.dll) of the mod.
+* **Install the mod [UIExpansionKit](https://github.com/knah/VRCMods) by [knah](https://github.com/knah), it is required.**
+* Drag/copy the DLL files that you have downloaded into the Mods folder.
+* That's it! Now just run the game and the mod should be installed!
+
+## How to use
+* Open the Quick menu and click the "Tracking rotation" button.
+* You can change rotation value, high precision value and "Reset rotation when a new world loads" in Mod settings (Quick menu -> Settings -> Mod settings).
---
+++ TrackingRotatorMod.cs
@@ -0,0 +1,96 @@
+﻿using MelonLoader;
+using UIExpansionKit.API;
+using UnityEngine;
+
+namespace TrackingRotator {
+
+    public static class ModBuildInfo {
+        public const string Name = "TrackingRotator";
+        public const string Author = "nitro.";
+        public const string Version = "1.0.0";
+        public const string DownloadLink = "https://github.com/nitrog0d/TrackingRotator/releases/latest/download/TrackingRotator.dll";
+        public const string GameDeveloper = "VRChat";
+        public const string Game = "VRChat";
+    }
+
+    public class TrackingRotatorMod : MelonMod {
+        private const string ModCategory = "TrackingRotator";
+        private const string RotationValuePref = "RotationValue";
+        private const string HighPrecisionRotationValuePref = "HighPrecisionRotationValue";
+        private const string ResetRotationOnSceneChangePref = "ResetRotationOnSceneChange";
+
+        private static float rotationValue = 0f;
+        private static float highPrecisionRotationValue = 0f;
+        private static bool highPrecision = false;
+        private static bool resetRotationOnSceneChange = false;
+
+        private static Transform cameraTransform = null;
+        private static Quaternion originalRotation;
+
+        public override void OnApplicationStart() {
+            MelonLogger.Msg("Mod loaded.");
+            MelonPreferences.CreateCategory(ModCategory, "Tracking Rotator");
+            MelonPreferences.CreateEntry(ModCategory, RotationValuePref, 22.5f, "Rotation value");
+            MelonPreferences.CreateEntry(ModCategory, HighPrecisionRotationValuePref, 1f, "High precision rotation value");
+            MelonPreferences.CreateEntry(ModCategory, ResetRotationOnSceneChangePref, false, "Reset rotation when a new world loads");
+            ExpansionKitApi.GetExpandedMenu(ExpandedMenu.QuickMenu).AddSimpleButton("Tracking rotation", ShowRotationMenu);
+            OnPreferencesSaved();
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName) {
+            if (resetRotationOnSceneChange) {
+                if (cameraTransform) {
+                    cameraTransform.localRotation = originalRotation;
+                }
+            }
+        }
+
+        public override void OnPreferencesSaved() {
+            rotationValue = MelonPreferences.GetEntryValue<float>(ModCategory, RotationValuePref);
+            highPrecisionRotationValue = MelonPreferences.GetEntryValue<float>(ModCategory, HighPrecisionRotationValuePref);
+            resetRotationOnSceneChange = MelonPreferences.GetEntryValue<bool>(ModCategory, ResetRotationOnSceneChangePref);
+        }
+
+        public override void VRChat_OnUiManagerInit() {
+            cameraTransform = Object.FindObjectOfType<VRCVrCameraSteam>().field_Public_Transform_0;
+            originalRotation = cameraTransform.localRotation;
+        }
+
+        private static ICustomShowableLayoutedMenu rotationMenu = null;
+
+        // Based on knah's ViewPointTweaker mod, https://github.com/knah/VRCMods/blob/master/ViewPointTweaker
+        private void ShowRotationMenu() {
+            if (rotationMenu == null) {
+                rotationMenu = ExpansionKitApi.CreateCustomQuickMenuPage(LayoutDescription.QuickMenu4Columns);
+
+                void Move(Vector3 direction) {
+                    cameraTransform.Rotate(direction, highPrecision ? highPrecisionRotationValue : rotationValue, Space.World);
+                }
+
+                var transform = Camera.main.transform;
+
+                rotationMenu.AddSpacer();
+                rotationMenu.AddSimpleButton("Forward", () => Move(transform.right));
+                rotationMenu.AddSpacer();
+                rotationMenu.AddSpacer();
+
+                rotationMenu.AddSimpleButton("Tilt Left", () => Move(transform.forward));
+                rotationMenu.AddSimpleButton("Reset", () => cameraTransform.localRotation = originalRotation);
+                rotationMenu.AddSimpleButton("Tilt Right", () => Move(-transform.forward));
+                rotationMenu.AddSpacer();
+
+                rotationMenu.AddSpacer();
+                rotationMenu.AddSimpleButton("Backward", () => Move(-transform.right));
+                rotationMenu.AddSimpleButton("Left", () => Move(-transform.up));
+                rotationMenu.AddSimpleButton("Right", () => Move(transform.up));
+
+                rotationMenu.AddToggleButton("High precision", b => highPrecision = b, () => highPrecision);
+                rotationMenu.AddSpacer();
+                rotationMenu.AddSpacer();
+                rotationMenu.AddSimpleButton("Back", rotationMenu.Hide);
+            }
+
+            rotationMenu.Show();
+        }
+    }
+}
