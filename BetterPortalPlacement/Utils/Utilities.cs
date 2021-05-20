a519b6e8c15177bbc875a35b0aefeb203aa6b27f
Davi <48662592+d-mageek@users.noreply.github.com>
2021-05-20 01:38:40 -0300

    Version proofing patches

--- BetterPortalPlacement/Utils/Utilities.cs
+++ BetterPortalPlacement/Utils/Utilities.cs
@@ -75,14 +75,17 @@ namespace BetterPortalPlacement.Utils
         {
             get
             {
-                return typeof(PortalInternal).GetMethod(nameof(PortalInternal.Method_Public_Static_Boolean_ApiWorld_ApiWorldInstance_Vector3_Vector3_Boolean_0));
+                return typeof(PortalInternal).GetMethods()
+                    .Where(method => method.Name.StartsWith("Method_Public_Static_Boolean_ApiWorld_ApiWorldInstance_Vector3_Vector3_Boolean_") && !method.Name.Contains("_PDM_"))
+                    .OrderBy(method => UnhollowerSupport.GetIl2CppMethodCallerCount(method)).Last();
             }
         }
         
         public static void ApplyPatches()
         {
             Main.HarmonyInstance.Patch(CreatePortalMethod, new HarmonyMethod(typeof(Main).GetMethod(nameof(Main.OnPortalCreated))));
-            Main.HarmonyInstance.Patch(typeof(VRCUiPopupManager).GetMethod(nameof(VRCUiPopupManager.Method_Public_Void_String_String_Single_0)), 
+            Main.HarmonyInstance.Patch(typeof(VRCUiPopupManager).GetMethods()
+                    .Where(method => method.Name.StartsWith("Method_Public_Void_String_String_Single_") && !method.Name.Contains("_PDM_")).Last(),
                 new HarmonyMethod(typeof(Main).GetMethod(nameof(Main.ShowAlert))));
         }
 
