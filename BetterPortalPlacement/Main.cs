2ceb890ffa9fbdff71dd081ee7f7aaeb6a290bc3
Elaina <48662592+oestradiol@users.noreply.github.com>
2024-08-16 00:31:35 -0300

    Version Bump

--- BetterPortalPlacement/Main.cs
+++ BetterPortalPlacement/Main.cs
@@ -22,7 +22,7 @@ namespace BetterPortalPlacement
     {
         public const string Name = "BetterPortalPlacement";
         public const string Author = "Elaina";
-        public const string Version = "1.0.5";
+        public const string Version = "1.0.6";
     }
 
     internal static class UIXManager { public static void OnApplicationStart() => UIExpansionKit.API.ExpansionKitApi.OnUiManagerInit += Main.VRChat_OnUiManagerInit; }
