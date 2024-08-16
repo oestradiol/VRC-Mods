f4bf510a79f8f5603efdbad0031391b9f1449e5c
Elaina <48662592+oestradiol@users.noreply.github.com>
2024-08-16 00:30:32 -0300

    Bump version :)

--- BetterPortalPlacement/Main.cs
+++ BetterPortalPlacement/Main.cs
@@ -22,7 +22,7 @@ namespace BetterPortalPlacement
     {
         public const string Name = "BetterPortalPlacement";
         public const string Author = "Elaina";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
     }
 
     public class Main : MelonMod
